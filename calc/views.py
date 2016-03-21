2a2c437a2151f343a214a7bf16d25912be5b500b
Eloy Navarro Morales <eloynavarrom@gmail.com>
2016-03-21 19:20:47 +0100

    Primer intento

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def suma(request, num1, num2):
+    return HttpResponse(num1+'+'+num2+'='+str(int(num1)+int(num2)))
+
+def resta(request, num1, num2):
+    return HttpResponse(num1+'-'+num2+'='+str(int(num1)-int(num2)))
+
+def multiplica(request, num1, num2):
+    return HttpResponse(num1+'*'+num2+'='+str(int(num1)*int(num2)))
+
+def divide(request, num1, num2):
+    return HttpResponse(num1+'/'+num2+'='+str(int(num1)/int(num2)))
+
+def la404(request):
+    return HttpResponse('Not Found')
